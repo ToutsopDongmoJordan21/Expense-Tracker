e80a8b8655522fd2b59723732e813605a301ed07
ToutsopDongmoJordan21 <toutsopjordan1999@gmail.com>
2021-10-12 12:27:44 +0100

    end of tutorial

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -4,7 +4,13 @@ export default (state, action) => {
             return {
                 ...state,
                 transactions: state.transactions.filter(Transaction => Transaction.id !==
-                    action.payload)
+                action.payload)
+            }
+
+        case 'ADD_TRANSACTION':
+            return {
+                ...state,
+                transactions: [action.payload, ...state.transactions]
             }
         default:
             return state;
